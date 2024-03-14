a4f99a2df1360976000cfd01a3859d741e5b01fc
luiscmicro <luis_cantu@berkeley.edu>
2024-03-13 17:04:20 -0700

    Adding new database migration

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00ef63d92a2b7a1541a93a306f93fd3ba08e938b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+174649a8ec947c3073c28463b4af93d793167831")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
